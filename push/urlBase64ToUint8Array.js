6f196383ff49a516bc5fb260d5a6790cfe4267a7
peccu <peccul+github@gmail.com>
2023-04-14 22:34:46 +0900

    Update urlBase64ToUint8Array.js

--- push/urlBase64ToUint8Array.js
+++ push/urlBase64ToUint8Array.js
@@ -1,4 +1,4 @@
-export default (base64String) => {
+export const urlBase64ToUint8Array = (base64String) => {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, "+")
@@ -9,4 +9,4 @@ export default (base64String) => {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
-}
+};
